04538097d5043a45be614a6252013fd5dfd10483
DanielTS5 <danielt.soriano@gmail.com>
2025-09-18 21:50:18 -0600

    curso8

---
+++ leccion8/src/main/java/com/curso/leccion8/Leccion8.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.curso.leccion8;
+
+/**
+ *
+ * @author danie
+ */
+public class Leccion8 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
