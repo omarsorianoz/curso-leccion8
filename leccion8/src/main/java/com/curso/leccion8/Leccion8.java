90ebb4defc268e219cb093e89b72ef90193b23ca
DanielTS5 <danielt.soriano@gmail.com>
2025-09-18 22:09:23 -0600

    operaciones

--- leccion8/src/main/java/com/curso/leccion8/Leccion8.java
+++ leccion8/src/main/java/com/curso/leccion8/Leccion8.java
@@ -12,5 +12,17 @@ public class Leccion8 {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        suma(2,5);
+         suma(14,8);
+          suma(14,77);
+          System.out.println("la multiplicacion es :"+multiplicacion(3,3));
     }
+    public static void suma(int a,int b){
+        int c = a + b;
+        System.out.println("la suma es :" + c );
+    }
+    public static int multiplicacion(int a,int b){
+        return   a * b;
+    }
+            
 }
